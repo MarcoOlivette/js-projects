e6574b6babc6cc187ac790a7b37b8ab8743d8884
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-11-10 17:43:18 -0300

    funções geradoras

--- js-reborn/Advanced/Funções geradoras/main.js
+++ js-reborn/Advanced/Funções geradoras/main.js
@@ -8,3 +8,21 @@ function* geradora() {
 const g1 = geradora();
 
 console.log(g1.next().value)
+
+function* geradora1(){
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+function* geradora2(){
+    yield* geradora1();
+    yield 4;
+    yield 5;
+}
+
+const g = geradora2();
+
+for(let i of g){
+    console.log(i);
+}
